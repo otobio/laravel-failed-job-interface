beb91b10613db12d0c8c5e85c5baed82660a1aac
Donny5300 <wavangrondelle@live.nl>
2018-08-21 23:28:31 +0200

    Update config. Add instructions

--- src/config/failed_job_interface.php
+++ src/config/failed_job_interface.php
@@ -1,7 +1,17 @@
 <?php return [
+
+    # Specify the URL for the interface
     'url'             => 'failed-job-interface',
+
+    # This middlewares will be called on the route group
     'middleware'      => [],
+
+    # Pagination limit
     'per_page'        => 20,
+
+    # Do you use Horizon? Null matches auto-detect
     'uses_horizon'    => null,
+
+    # If you load the interface, do you want the filters collapsed?
     'display_filters' => false
 ];
